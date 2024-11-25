10b55197f8a2d0e27a6954ff905b2b91ed937f95
bjp333 <gray.johansson@protonmail.com>
2024-11-24 20:52:04 -0800

    Update LimitOrderHook.sol

--- src/LimitOrderHook.sol
+++ src/LimitOrderHook.sol
@@ -6,19 +6,20 @@ import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {Currency} from "v4-core/types/Currency.sol";
 import {CurrencySettler} from "@uniswap/v4-core/test/utils/CurrencySettler.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
-import {BeforeSwapDelta, toBeforeSwapDelta} from "v4-core/types/BeforeSwapDelta.sol";
 import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
+import {IERC6909} from "@openzeppelin/contracts/interfaces/IERC6909.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 
 
-contract LimitOrderHook is BaseHook {
+contract LimitOrderHook is BaseHook, IERC6909 {
+    using EnumerableSet for EnumerableSet.Int24Set;
     using CurrencySettler for Currency;
 
 
     // Struct to represent a limit order
     struct LimitOrder {
-        address recipient;      // Who receives the filled order
+        address owner;      // Who receives the filled order
         bool isToken0;         // Whether the input token is token0
         bool isRange;          // Whether this is a range order or single-tick limit order
         int24 targetTick;      // Target tick for single-tick limit orders
@@ -27,22 +28,24 @@ contract LimitOrderHook is BaseHook {
         int24 bottomTick;      // Bottom tick for range orders
         int24 topTick;         // Top tick for range orders
         bool executed;         // Whether the order has been executed 
+        uint256 executionFees0; // Fees earned in token0
+        uint256 executionFees1;  // Fees earned in token1
     }
 
-    struct VirtualPool {
-        // Mirror of all pool liquidity
-        mapping(int24 => uint128) liquidityPerTick;
+    // Mapping from poolId to tick to orderIds
+    mapping(bytes32 => mapping(int24 => bytes32[])) public tickToOrders;
+    
+    // All limit orders
+    mapping(bytes32 => LimitOrder) public limitOrders;
+    
+    // Track which ticks have limit orders
+    mapping(bytes32 => EnumerableSet.Int24Set) private poolTicks;
 
-        // Additional state for limit orders
-        mapping(bytes32 => LimitOrder) limitOrders;
+    // ERC-6909 claim tokens
+    mapping(address => mapping(uint256 => uint256)) public balanceOf;
+    mapping(address => mapping(address => bool)) public isOperator;
 
-        // Track which ticks have limit orders using EnumerableSet
-        EnumerableSet.Int24Set limitOrderTicks; 
-        // Current virtual price (might differ from real pool after executions)
-        int24 currentTick;
-    }
-
-    mapping(bytes32 => VirtualPool) public virtualPools;
+    uint256 public constant CLAIM_TOKEN_ID = 1;
 
     constructor(IPoolManager poolManager) BaseHook(poolManager) {}
 
@@ -50,177 +53,150 @@ contract LimitOrderHook is BaseHook {
         return Hooks.Permissions({
             beforeInitialize: false,
             afterInitialize: false,
-            beforeAddLiquidity: true,  // We want to intercept all liquidity adds
+            beforeAddLiquidity: false,  
             afterAddLiquidity: false,
-            beforeRemoveLiquidity: false,
+            beforeRemoveLiquidity: false, 
             afterRemoveLiquidity: false,
-            beforeSwap: false,
-            afterSwap: false,
+            beforeSwap: true,  // Need to record old tick
+            afterSwap: true,  // Execute limit orders
             beforeDonate: false,
             afterDonate: false,
             beforeSwapReturnDelta: false,
-            afterSwapReturnDelta: true,  // Adjust swap outputs here
+            afterSwapReturnDelta: false,  
             afterAddLiquidityReturnDelta: false, 
-            afterRemoveLiquidityReturnDelta: true // Adjust removal amounts based on execution state
+            afterRemoveLiquidityReturnDelta: false
         });
     }
 
-    // Intercept all liquidity adds to mirror in virtual pool
-    function beforeAddLiquidity(
-        address sender,
-        PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata params,
-        bytes calldata data
+    // Store old tick in transient storage
+    function beforeSwap(
+        address,
+        PoolKey calldata key, 
+        IPoolManager.SwapParams calldata,
+        bytes calldata
     ) external override returns (bytes4) {
-        bytes32 poolId = keccak256(abi.encode(key));
-        VirtualPool storage vPool = virtualPools[poolId];
-        
-        // Mirror liquidity in virtual pool
-        vPool.totalLiquidityPerTick[params.tickLower] += uint128(params.liquidityDelta);
-        vPool.totalLiquidityPerTick[params.tickUpper] += uint128(params.liquidityDelta);
-
-        // If this is a limit order, track additional info
-        if (data.length > 0) {
-            (bool isLimitOrder, int24 targetTick, bool isRange, int24 bottomTick, int24 topTick) = 
-                abi.decode(data, (bool, int24, bool, int24, int24));
-            
-            if (isLimitOrder) {
-                bytes32 orderId = keccak256(abi.encode(sender, block.timestamp, params));
-                vPool.limitOrders[orderId] = LimitOrder({
-                    recipient: sender,
-                    isToken0: params.tickLower < vPool.currentTick,
-                    isRange: isRange,
-                    targetTick: targetTick,
-                    amount: 0, // Will be set after add liquidity
-                    liquidity: uint128(params.liquidityDelta),
-                    bottomTick: bottomTick,
-                    topTick: topTick,
-                    executed: false
-                });
-
-                if (isRange) {
-                    vPool.isLimitOrderTick[bottomTick] = true;
-                    vPool.isLimitOrderTick[topTick] = true;
-                } else {
-                    vPool.isLimitOrderTick[targetTick] = true;
-                }
-            }
+        (,int24 oldTick,,,,,) = poolManager.getSlot0(key.toId());
+        assembly {
+            tstore(0x4444000000000000000000000000000000000000000000000000000000000000, oldTick)
         }
-
-        return BaseHook.beforeAddLiquidity.selector;
+        return BaseHook.beforeSwap.selector;
     }
 
-    function afterSwapReturnDelta(
+    function afterSwap(
         address,
         PoolKey calldata key,
         IPoolManager.SwapParams calldata params,
-        BalanceDelta realDelta,
         bytes calldata
-    ) external override returns (BalanceDelta) {
-        bytes32 poolId = keccak256(abi.encode(key));
-        VirtualPool storage vPool = virtualPools[poolId];
+    ) external override returns (bytes4) {
+        bytes32 poolId = key.toId();
 
-        // Store old tick for checking crossed limit orders
-        int24 oldTick = vPool.currentTick;
-        
-        // Execute swap in virtual pool and get virtual amounts
-        (int256 virtualAmount0, int256 virtualAmount1) = executeVirtualSwap(
-            vPool,
-            params,
-            oldTick,
-            realDelta
-        );
+        // Get old and new tick
+        int24 oldTick;
+        assembly {
+            oldTick := tload(0x4444000000000000000000000000000000000000000000000000000000000000)
+        }
+        (,int24 newTick,,,,,)=poolManager.getSlot0(poolId);
 
-        // Return difference between virtual and real amounts
-        return BalanceDelta.wrap(
-            virtualAmount0 - realDelta.amount0(),
-            virtualAmount1 - realDelta.amount1()
-        );
-    }
+        // Determine tick range to check
 
-    function executeVirtualSwap(
-        VirtualPool storage vPool,
-        IPoolManager.SwapParams calldata params,
-        int24 oldTick,
-        BalanceDelta realDelta
-    ) internal returns (int256 virtualAmount0, int256 virtualAmount1) {
-        vPool.currentTick = computeNewTick(params, realDelta);
-        
-        // Get range of ticks to check
-        int24 startTick = params.zeroForOne ? oldTick : vPool.currentTick;
-        int24 endTick = params.zeroForOne ? vPool.currentTick : oldTick;
-        
-        // Use EnumerableSet to efficiently find relevant ticks
-        uint256 length = vPool.limitOrderTicks.length();
-        for (uint256 i = 0; i < length; ) {
-            int24 tick = vPool.limitOrderTicks.at(i);
-            
-            // Check if tick is in our range
+        int24 startTick = params.zeroForOne ? oldTick : newTick;
+        int24 endTick = params.zeroForOne ? newTick : oldTick;
+
+        // Check all ticks in range for limit orders
+        uint256 length = poolTicks[poolId].length();
+        for (uint256 i = 0; i < length;) {
+            int24 tick = poolTicks[poolId].length();
+
             if (tick >= startTick && tick <= endTick) {
-                executeOrdersAtTick(vPool, tick);
-                // Optional: remove tick if all orders executed
-                if (allOrdersExecutedAtTick(vPool, tick)) {
-                    vPool.limitOrderTicks.remove(tick);
-                    // Don't increment i since we removed an element
-                    continue;
+                // Execute orders at this tick
+                bytes32[] storage orderIds = tickToOrders[poolId][tick];
+
+                for(uint256 j = 0; j < orderIds.length; j++) {
+                    LimitOrder storage order = limitOrders[orderIds[j]];
+
+                    if (!order.executed && tick == order.targetTick) {
+                        // Burn the liquidity
+                        _burnLimitOrder(key, order, orderIds[j]);
+
+                        // Calculate execution amounts and mint claim token
+                        (uint256 amount0, uint256 amount1) = _calculateExecutionAmounts(order);
+                        balanceOf[order.owner][CLAIM_TOKEN_ID] += amount0 + amount1;
+
+                        emit LimitOrderExecuted(orderIds[j], order.owner, amount0, amount1);
+                    }                
                 }
             }
-            unchecked { ++i; }
+            unchecked { ++i;}
         }
-        
-        return calculateVirtualSwapAmounts(vPool, params, realDelta);
+        return BaseHook.afterSwap.selector;
     }
 
-    function afterRemoveLiquidityReturnDelta(
-        address sender,
+    function _burnLimitOrder(
         PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata params,
-        BalanceDelta realDelta,
-        bytes calldata hookData
-    ) external override returns (BalanceDelta) {
-        bytes32 poolId = keccak256(abi.encode(key));
-        VirtualPool storage vPool = virtualPools[poolId];
-        
-        // Update virtual pool total liquidity
-        vPool.totalLiquidityPerTick[params.tickLower] -= uint128(params.liquidityDelta);
-        vPool.totalLiquidityPerTick[params.tickUpper] -= uint128(params.liquidityDelta);
+        LimitOrder storage order,
+        bytes32 orderId
+    ) internal {
+        // Burn liquidity from pool
+        poolManager.modifyLiquidity(
+            key,
+            IPoolManager.ModifyLIquidityParams({
+                tickLower: order.bottomTick,
+                tickUpper: order.topTick,
+                liquidityDelta: -int128(order.liquidity)
+            })
+        );
 
-        // If this is a limit order, calculate based on execution state
-        if (hookData.length > 0) {
-            bytes32 orderId = abi.decode(hookData, (bytes32));
-            LimitOrder storage order = vPool.limitOrders[orderId];
-            
-            if (order.executed) {
-                // Calculate amounts as if executed at target price
-                (int256 virtualAmount0, int256 virtualAmount1) = 
-                    calculateExecutedAmounts(order, uint128(-params.liquidityDelta));
-                
-                return BalanceDelta.wrap(
-                    virtualAmount0 - realDelta.amount0(),
-                    virtualAmount1 - realDelta.amount1()
-                );
-            }
-        }
-
-        return BalanceDelta.wrap(0, 0);
+        order.executed = true;
     }
 
-    function placeLimitOrder(...) {
-        // ... other logic ...
-        
-        // Add to EnumerableSet instead of mapping
-        if (isRange) {
-            vPool.limitOrderTicks.add(bottomTick);
-            vPool.limitOrderTicks.add(topTick);
-        } else {
-            vPool.limitOrderTicks.add(targetTick);
-        }
+    // ERC-6909 functions
+    function setOperator(address operator, bool approved) external returns (bool) {
+        isOperator[msg.sender][operator] = approved;
+        return true;
     }
-    // Helper functions to implement:
-    // - computeNewTick()
-    // - executeOrdersAtTick()
-    // - calculateVirtualSwapAmounts()
-    // - calculateExecutedAmounts()
+
+    function transfer(address to, uint256 id, uint256 amount) external returns (bool) {
+        balanceOf[msg.sender][id] -= amount;
+        balanceOf[to][id] += amount;
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to, 
+        uint256 amount
+    ) external returns (bool) {
+        require(
+            from == msg.sender || isOperator[from][msg.sender],
+            "Not authorized"
+        );
+        balanceOf[from][id] -= amount;
+        balanceOf[to][id] += amount;
+        return true;
+    }
+
+    // Function to claim executed limit orders
+    function claimLimitOrder(bytes32 orderId) external {
+        LimitOrder storage order = limitOrders[orderId];
+        require(order.executed, "Order not executed");
+        require(msg.sender == order.owner, "Not owner");
+
+        uint256 claimAmount = balanceOf[msg.sender][CLAIM_TOKEN_ID];
+        require(claimAmount > 0, "Nothing to claim");
+
+        balanceOf[msg.sender][CLAIM_TOKEN_ID] = 0;
+
+        // Transfer tokens to owner
+        // Implement actual token transfer logic
+    } 
+
+    event LimitOredrExecuted(
+        bytes32 indexed orderId,
+        address indexed owner,
+        uint256 amount0,
+        uint256 amount1
+    );
+    
 }
 
 
